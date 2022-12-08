e34f7e14821816649bd08bbfb72c8490bf6a7a15
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2022-12-08 16:06:10 -0500

    Add starting screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,91 @@
 let jumping = 0;
 let gaming = 0;
+let gameInerval;
+
+const start = () => {
+    gaming = 1;
+    $("#start-screen").hide();
+    $("#game-bird").show();
+    $("#block").addClass("animateObstacle");
+    $("#hole").addClass("animateObstacle");
+    $("#block").css("animationPlayState", "running");
+    $("#hole").css("animationPlayState", "running");
+    gameInterval = setInterval(setGameInterval, 10);
+};
+
+const gameOver = (birdPosition) => {
+    //Game Over 
+    gaming = 0;
+    clearInterval(gameInterval);
+
+    //Stop all animations
+    $("#block").css("animationPlayState", "paused");
+    $("#hole").css("animationPlayState", "paused");
+
+    //Animate Bird
+    $(":root").css({ "--bird-game-over": birdPosition.top - 100 + "px" });
+    $("#game-bird").addClass("game-over");
+    gameOverTimeout = setTimeout(function() {
+        $("#game-bird").hide();
+        $("#endScreen").show();
+    }, 900);
+};
 
 const jump = () => {
     let jumpTimeout;
     if (jumping){
-        $("#bird").removeClass("jumping")
+        $("#game-bird").removeClass("jumping")
         clearInterval(jumpTimeout)
         jumping = 0;
     } 
     jumping = 1;
-    let position = $("#bird").position().top;
+    let position = $("#game-bird").position().top;
     $(":root").css({"--bird-jump-start": position+"px", "--bird-jump-end": position-150+"px"});
-    $("#bird").addClass("jumping")
+    $("#game-bird").addClass("jumping")
     jumpTimeout = setTimeout(function() {
-        $("#bird").removeClass("jumping")
+        $("#game-bird").removeClass("jumping")
         jumping = 0;
     }, 250)
 }
 
+const setGameInterval = () => {
+    // bird falling
+    $("#game-bird").css("top", $("#game-bird").position().top + 3 + "px");
+
+    // Game Logic
+    let _birdposition = $("#game-bird").position();
+    console.log(_birdposition)
+    let birdPosition = {
+        top: _birdposition.top,
+        bottom: _birdposition.top + $("#game-bird").height(),
+        left: 150,
+        right: 150 + $("#game-bird").width(),
+    };
+
+    //check whether a block is hitted.
+    let _blockPosition = $("#block").position();
+    let blockPosition = {
+        left: _blockPosition.left,
+        right: _blockPosition.left + $("#hole").width()
+    };
+
+    let _holePosition = $("#hole").position();
+    let holePosition = {
+        top: _holePosition.top,
+        bottom: _holePosition.top + $("#hole").height()
+    };
+
+    //If hit ceiling or floor
+    if (birdPosition.bottom >= 600 || birdPosition.top <= 0) gameOver(birdPosition, gameInterval);
+
+    // While birds not hit the block, return empty
+    if (birdPosition.right < blockPosition.left || birdPosition.left > blockPosition.right) return;
+
+    // While birds is in the holes
+    if (birdPosition.bottom > holePosition.top && birdPosition.top < holePosition.bottom) return;
+
+    gameOver(birdPosition);
+}
 const loadPage = () => {
     
     $("#hole").on("animationiteration", function() {
@@ -26,79 +94,13 @@ const loadPage = () => {
         $(this).css("top", randomTop);
     })
 
-
-    const restart = () => {
-        gaming = 1;
-
-
-    }
-
-    const gameOver = (birdPosition) => {
-        //Game Over 
-        gaming = 0;
-        clearInterval(gameInterval);
-    
-        //Stop all animations
-        $("#block").css("animationPlayState", "paused");
-        $("#hole").css("animationPlayState", "paused");
-    
-        //Animate Bird
-        $(":root").css({ "--bird-game-over": birdPosition.top - 100 + "px" });
-        $("#bird").addClass("game-over");
-        gameOverTimeout = setTimeout(function() {
-            $("#bird").hide();
-            $("#endScreen").show();
-        }, 900);
-
-        restart();
-    }
-
-    let gameInterval = setInterval(function(){
-        // bird falling
-        $("#bird").css("top", $("#bird").position().top + 3 + "px");
-
-        // Game Logic
-        let _birdposition = $("#bird").position();
-        let birdPosition = {
-            top: _birdposition.top,
-            bottom: _birdposition.top + $("#bird").height(),
-            left: 150,
-            right: 150 + $("#bird").width(),
-        };
-
-        //check whether a block is hitted.
-        let _blockPosition = $("#block").position();
-        let blockPosition = {
-            left: _blockPosition.left,
-            right: _blockPosition.left + $("#hole").width()
-        };
-
-        let _holePosition = $("#hole").position();
-        let holePosition = {
-            top: _holePosition.top,
-            bottom: _holePosition.top + $("#hole").height()
-        };
-
-        //If hit ceiling or floor
-        if (birdPosition.bottom >= 600 || birdPosition.top <= 0)    gameOver(birdPosition);
-
-        // While birds not hit the block, return empty
-        if (birdPosition.right < blockPosition.left || birdPosition.left > blockPosition.right) return;
-
-        // While birds is in the holes
-        if(birdPosition.bottom > holePosition.top && birdPosition.top < holePosition.bottom) return;
-
-        gameOver(birdPosition);
-
-
-
-    }, 10)
-
     $("html").on("click", function(){
         jump();
     });
 
-    gaming = 1;
+    $("#start-button").on("click", function(){
+        start();
+    });
 }
 
 $(window).on("load", loadPage);
